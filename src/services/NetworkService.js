4203184281aa27088e82da789e1d722521c9532c
Blake Alan <blake.tiemann@gmail.com>
2023-08-09 17:11:54 -0500

    - feat: added network-speed to measure speed of all browser types

--- src/services/NetworkService.js
+++ src/services/NetworkService.js
@@ -1,22 +1,21 @@
 import { reactive } from 'vue'
+const NetworkSpeed = require('network-speed');
 
 const NetworkService = reactive({
   supportsVideo: false,
-  effectiveType: null, // slow-2g, 2g, 3g, 5g
-  boot() {
-    /**
-     *  Unsupported: Safari, Firefox
-     */
-    if (navigator.connection) {
-      this.updateEffectiveSpeed()
-      console.log('NetworkService@boot', 'supportsVideo: ', this.supportsVideo)
-      navigator.connection.onchange = this.updateEffectiveSpeed()
-    }
-  },
-  updateEffectiveSpeed() {
-    this.effectiveType = navigator.connection.effectiveType ?? null
+  downloadSpeed: 0, // Mbps
+  cdnCheckUrl: 'https://eu.httpbin.org/stream-bytes/500000',
+  async boot() {
+    await this.getNetworkDownloadSpeed() // Safari, Firefox
 
-    this.supportsVideo = ['3g', '4g'].includes(navigator.connection.effectiveType)
+    console.log('NetworkService@boot', 'downloadSpeed', this.downloadSpeed, 'supportsVideo: ', this.supportsVideo)
+  },
+  async getNetworkDownloadSpeed() {
+    const fileSizeInBytes = 500000;
+    const speed = await (new NetworkSpeed()).checkDownloadSpeed(this.cdnCheckUrl, fileSizeInBytes);
+
+    this.downloadSpeed = parseFloat(speed.mbps)
+    this.supportsVideo = this.downloadSpeed >= 5
   }
 })
 
